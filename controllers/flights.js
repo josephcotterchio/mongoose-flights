6a0e66fdce53d12651eacac425f53e0f5eade660
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-14 19:03:54 -0400

    "adding flights"

--- controllers/flights.js
+++ controllers/flights.js
@@ -3,23 +3,23 @@ var Ticket = require("../models/ticket");
 
 function index(req, res) {
 Flight.find({}, function (err, flights) {
-res.render('flights/flight', { title: 'All Flights', flights });
+res.render('/flight', { title: 'All Flights', flights });
 });
  }
 
 function show(req, res) {
 Flight.findById(req.params.id, function (err, flight) {
-res.render('flights/show', { title: 'Flight Detail', flight });
+res.render('/show', { title: 'Flight Detail', flight });
  });
  }
 
 function newFlight(req, res) {
-res.render('flights/new', { title: 'Add Flight' });
+res.render('/new', { title: 'Add Flight' });
 }
 
 function create(req, res) {
     Flight.create(req.body);
-    res.redirect("/flights");
+    res.redirect("/flight");
 }
 
 module.exports = {
--- routes/flights.js
+++ routes/flights.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-let flightsCtrl = require("../controllers/flights");
+var flightsCtrl = require('../controllers/flights')
 
 /* GET users listing. */
-router.get('/', flightsCtrl.index);
+router.get('/flight', flightsCtrl.index);
 router.get('/show', flightsCtrl.show);
-router.post('/', flightsCtrl.create);
+router.post('/new', flightsCtrl.create);
 
 module.exports = router;
