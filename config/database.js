37571be12f21ecba11b39b165ca11af4d80a9aa4
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-14 19:54:07 -0400

    "without partials"

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/flights', {
+mongoose.connect('mongodb://localhost/', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
