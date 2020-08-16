cb65fc67aa14a35af806e35c6d59143502f95550
Joe Cotterchio <joecotterchio@Joes-MacBook-Air.local>
2020-08-16 17:04:54 -0400

    "updates"

--- models/ticket.js
+++ models/ticket.js
@@ -1,20 +1,19 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ticketSchema = new Schema({
-    seat: {
-        type: String,
-        match: (/[A-F][1-9]\d?/),
-    },
-    price: {
-        type: Number,
-        min: 0,
-    },
-    flight: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Flight',
-    },
+  seat: {
+    type: String,
+    match: /[A-Z][1-9]\d?/,
+  },
+  price: {
+    type: Number,
+    min: 0,
+  },
+  flight: {
+    type: mongoose.Types.ObjectId,
+    ref: "Flight",
+  },
 });
 
-
-module.exports = mongoose.model('Ticket', ticketSchema);
+module.exports = mongoose.model("Ticket", ticketSchema);
